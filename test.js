b5e24762a9bf5d6119b897cf4e6fbec3a6e62145
Adrien ERAUD <adrien@lebondrive.fr>
2020-07-07 12:57:58 +0200

    Add cropping feature

--- test.js
+++ test.js
@@ -4,12 +4,28 @@ const isGif = require("is-gif");
 const test = require("ava");
 const gifsicle = require(".");
 
-test("Resize", async t => {
+test("Resize (withoutEnlargement: true)", async t => {
 	await gifsicle(path.join(__dirname, "test.gif"))
 		.resize(600, 600, { kernel: gifsicle.kernel.lanczos3, withoutEnlargement: true })
-		.toFile(path.join(__dirname, "test-resized.gif"));
+		.toFile(path.join(__dirname, "test-resized-fit.gif"));
 
-	t.true(isGif(fs.readFileSync(path.join(__dirname, "test-resized.gif"))));
+	t.true(isGif(fs.readFileSync(path.join(__dirname, "test-resized-fit.gif"))));
+});
+
+test("Resize (withoutEnlargement: false)", async t => {
+	await gifsicle(path.join(__dirname, "test.gif"))
+		.resize(600, 600, { kernel: gifsicle.kernel.lanczos3, withoutEnlargement: false })
+		.toFile(path.join(__dirname, "test-resized-touch.gif"));
+
+	t.true(isGif(fs.readFileSync(path.join(__dirname, "test-resized-touch.gif"))));
+});
+
+test("Crop", async t => {
+	await gifsicle(path.join(__dirname, "test.gif"))
+		.crop(600, 600, { position: gifsicle.position.center })
+		.toFile(path.join(__dirname, "test-cropped.gif"));
+
+	t.true(isGif(fs.readFileSync(path.join(__dirname, "test-cropped.gif"))));
 });
 
 test("Greyscale", async t => {
