fa101d5ac1b22bfde206dd71aecfeb05faaea8e5
Adrien ERAUD <e-adrien@users.noreply.github.com>
2024-02-01 13:51:26 +0100

    Update prettier configuration

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,9 +3,15 @@ const config = {
   trailingComma: "es5",
   overrides: [
     {
-      files: [".eslintrc.js", ".prettierrc.js", "*.json", "*.md"],
+      files: [".eslintrc.*", ".prettierrc.*", "*.json", "*.md"],
       options: {
         printWidth: 80,
+      },
+    },
+    {
+      files: ["tsconfig.json"],
+      options: {
+        trailingComma: "none",
       },
     },
   ],
